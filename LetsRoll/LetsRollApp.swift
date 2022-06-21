9d734dd816024c5dccc0b2ef2bdd66d0c4383543
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-06-21 17:37:55 +0300

    Initial Commit

---
+++ LetsRoll/LetsRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsRollApp.swift
+//  LetsRoll
+//
+//  Created by Alex Po on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LetsRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
